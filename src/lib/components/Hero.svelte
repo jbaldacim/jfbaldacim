c2bc11f219107695986c76406649a6c386dde3ee
jbaldacim <jbaldacim@gmail.com>
2026-06-02 17:57:22 -0300

    refactor: update main layout and add Hero component

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,22 @@
+<section class="w-screen h-screen flex items-center justify-center">
+  <div class="max-w-7xl w-full">
+    <span class="text-sm font-light tracking-tight text-primary"
+      >Hey, there! I am</span
+    >
+    <h1
+      class="tracking-wider font-bold uppercase group-hover:text-primary transition-colors duration-300 text-7xl mt-8"
+    >
+      João<br />Baldacim
+    </h1>
+    <p class="mt-6 leading-relaxed max-w-xl">
+      A Full-Stack Developer building a career in Software Engineering. I geek
+      out over Front-End Development, Data Visualization, and Machine Learning —
+      turning ideas into functional and beautiful solutions.
+    </p>
+    <p class="mt-6 leading-relaxed max-w-xl">
+      Currently I'm a Systems Analysis and Development undergraduate student at
+      IFSP - Campus Boituva and I'm open to new opportunities for trainee and
+      junior positions.
+    </p>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
-<script lang="ts">
+<script>
+  import Hero from "$lib/components/Hero.svelte";
+</script>
+
+<!-- <script lang="ts">
   import About from "$lib/components/About.svelte";
   import Blog from "$lib/components/Blog.svelte";
   import Contact from "$lib/components/Contact.svelte";
   import Copyright from "$lib/components/Copyright.svelte";
-  import Currently from "$lib/components/Currently.svelte";
   import FeaturedProject from "$lib/components/FeaturedProject.svelte";
-  import FromTheBlog from "$lib/components/FromTheBlog.svelte";
   import Menu from "$lib/components/Menu.svelte";
-  import OpenTo from "$lib/components/OpenTo.svelte";
   import Projects from "$lib/components/Projects.svelte";
-  import Quote from "$lib/components/Quote.svelte";
   import Stack from "$lib/components/Stack.svelte";
 </script>
 
@@ -40,14 +40,14 @@
       <Contact class="col-span-3 row-span-1" href="/contact" />
 
       <Copyright class="col-span-3 row-span-1" />
-
-      <!-- <FromTheBlog class="col-span-3 row-span-2" /> -->
-
-      <!-- <Currently class="col-span-6 row-span-1" /> -->
-
-      <!-- <OpenTo class="col-span-3 row-span-1" /> -->
-
-      <!-- <Quote class="col-span-9 row-span-1" /> -->
     </section>
+  </main>
+</div> -->
+
+<div
+  class="bg-[radial-gradient(ellipse_80%_80%_at_25%_25%,transparent_40%,var(--color-background)_100%),radial-gradient(var(--dot-color)_1px,transparent_1px)] bg-size-[auto,24px_24px] flex items-center justify-center"
+>
+  <main class="">
+    <Hero />
   </main>
 </div>
