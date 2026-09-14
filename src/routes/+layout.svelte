303eec9762f9637613b417153ec30e249d87aae5
jbaldacim <jbaldacim@gmail.com>
2026-09-14 00:46:36 -0300

    style: change background and add Noise

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
-  import "./layout.css";
-  import favicon from "$lib/assets/favicon.svg";
-  import Menu from "$lib/components/Menu.svelte";
-  import Footer from "$lib/components/Footer.svelte";
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Menu from '$lib/components/Menu.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import Noise from '$lib/components/Noise.svelte';
 
-  let { children } = $props();
+	let { children } = $props();
 </script>
 
-<svelte:head
-  ><link rel="icon" href={favicon} /><title>João Baldacim</title></svelte:head
->
+<svelte:head><link rel="icon" href={favicon} /><title>João Baldacim</title></svelte:head>
 
-<div
+<!-- TODO - add noise instead of all black background -->
+<!-- <div
   class="bg-[radial-gradient(ellipse_80%_80%_at_25%_25%,transparent_40%,var(--color-background)_100%),radial-gradient(var(--dot-color)_1px,transparent_1px)] bg-size-[auto,24px_24px] min-h-screen flex flex-col pt-16"
->
-  <Menu />
-  {@render children()}
-  <Footer />
+> -->
+<div class="flex min-h-screen flex-col pt-16">
+	<Menu />
+	<Noise />
+	{@render children()}
+	<Footer />
 </div>
